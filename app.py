ff07c2ecf4dd850971d7a27b9e2631aa617cdd61
InsightEdge01 <131486782+InsightEdge01@users.noreply.github.com>
2023-11-10 20:56:09 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,52 +7,25 @@ from langchain.llms import OpenAI
 from langchain.chains import ConversationalRetrievalChain
 import openai
 import autogen
-from autogen import AssistantAgent, UserProxyAgent
-
-
-config_list = [
-    {
-         "api_base": "http://localhost:1234/v1",
-        "api_key": "NULL"
-    }
-]
-
-llm_config={
-    "request_timeout": 600,
-    "seed": 42,
-    "config_list": config_list,
-    "temperature": 0,
-    "functions": [
-        {
-            "name": "walmart",
-            "description": "Answer any walmart related questions",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "question": {
-                        "type": "string",
-                        "description": "The question to ask in relation to Walmart",
-                    }
-                },
-                "required": ["question"],
-            },
-        }
-    ],
-}
 
 
 
+
+#set llm for langchain using model from lmstudio
 openai.api_type = "open_ai"
 openai.api_base = "http://localhost:1234/v1"
 openai.api_key = "NULL"
 
-loaders = [PyPDFLoader('./walmart.pdf')]
+#load the pdf file from directory
+loaders = [PyPDFLoader('./chat_docs.pdf')]
 docs = []
-for l in loaders:
-    docs.extend(l.load())
+for file in loaders:
+    docs.extend(file.load())
+#split text to chunks
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000)
 docs = text_splitter.split_documents(docs)
 
+#create a vectorstore
 vectorstore = Chroma(
 collection_name="full_documents",
 embedding_function=HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2", 
@@ -66,37 +39,72 @@ qa = ConversationalRetrievalChain.from_llm(
     memory=ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 )
 
-#result = qa(({"question": "who is the CEO of walmart"}))
-#print(result['answer'])
 
-def walmart_docs(question):
+#set config for autogen
+config_list = [
+    {
+         "api_base": "http://localhost:1234/v1",
+        "api_key": "NULL"
+    }
+]
+
+#set autogen user agent and assistant agent with function calling
+llm_config={
+    "request_timeout": 600,
+    "seed": 42,
+    "config_list": config_list,
+    "temperature": 0,
+    "functions": [
+        {
+            "name": "chat_docs",
+            "description": "Answer any chat_docs related questions",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "question": {
+                        "type": "string",
+                        "description": "The question to ask in relation to chat_docs",
+                    }
+                },
+                "required": ["question"],
+            },
+        }
+    ],
+}
+
+#the function takes a parameter question,calls the qa chain and answer it by returnin the answer
+# from the chain
+def chat_docs(question):
     response = qa({"question": question})
     return response["answer"]
 
-# create an AssistantAgent instance named "assistant"
+
+# create an AssistantAgent instance "assistant"
 assistant = autogen.AssistantAgent(
     name="assistant",
     llm_config=llm_config,
 )
-# create a UserProxyAgent instance named "user_proxy"
+
+
+# create a UserProxyAgent instance "user_proxy"
 user_proxy = autogen.UserProxyAgent(
     name="user_proxy",
     human_input_mode="NEVER",
-    max_consecutive_auto_reply=10,
-    code_execution_config={"work_dir": "."},
+    max_consecutive_auto_reply=1,
+    code_execution_config={"work_dir": "docs"},
     llm_config=llm_config,
     system_message="""Reply TERMINATE if the task has been solved at full satisfaction.
 Otherwise, reply CONTINUE, or the reason why the task is not solved yet.""",
-    function_map={"walmart_docs":walmart_docs}
+    function_map={"chat_docs":chat_docs}
 )
 
 # the assistant receives a message from the user, which contains the task description
 user_proxy.initiate_chat(
     assistant,
     message="""
-Find the answers to the 3 questions below from the walmart.pdf and do not write any code.
+Find the answers to the 3 questions below from the chat_docs.pdf and do not write any code.
 
-1. Who is the CEO of walmart?
+1.Who is the CEO of walmart?
 2.What did Doug McMillon write about walmart.
 3.Write about the Executive Shuffle?
 
